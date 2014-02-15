52aadc0d3bd14181157b529d25f355ad0f684509
Nenad Radulović <nenad.b.radulovic@gmail.com>
2014-02-15 12:53:58 +0100

    File header is corrected

--- src/vtimer.c
+++ src/vtimer.c
@@ -1,10 +1,23 @@
-/******************************************************************************
- * This file is part of formula_display
+/*
+ * This file is part of eSolid.
  *
- * Copyright (C) 2011, 2012 - Nenad Radulovic
+ * Copyright (C) 2010 - 2013 Nenad Radulovic
  *
- * web site:    http://blueskynet.dyndns-server.com
- * e-mail  :    blueskyniss@gmail.com
+ * eSolid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * eSolid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with eSolid.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * web site:    http://github.com/nradulovic
+ * e-mail  :    nenad.b.radulovic@gmail.com
  *//***********************************************************************//**
  * @file
  * @author      Nenad Radulovic
